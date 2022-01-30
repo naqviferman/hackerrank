5f8444665d998081ddc8d6a9011a29c92aaa8371
Ferman Naqvi <naqviferman@gmail.com>
2022-01-31 00:46:44 +0530

    Test Cases

--- src/main/java/algorithms/implementation/TestKangaroo.java
+++ src/main/java/algorithms/implementation/TestKangaroo.java
@@ -2,4 +2,7 @@ package algorithms.implementation;
 
 // We can add some test cases here
 public class TestKangaroo {
+    public static void main(String[] args) {
+        System.out.println("Test cases");
+    }
 }
